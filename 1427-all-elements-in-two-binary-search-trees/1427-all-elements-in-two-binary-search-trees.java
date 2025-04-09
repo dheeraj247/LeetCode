0baa901b9f758407b3f52b7e71ffe92a69318191
dheeraj247 <dheerajsiripragada@gmail.com>
2025-04-09 22:07:50 +0530

    Time: 16 ms (87.75%), Space: 46.2 MB (70.52%) - LeetHub

--- 1427-all-elements-in-two-binary-search-trees/1427-all-elements-in-two-binary-search-trees.java
+++ 1427-all-elements-in-two-binary-search-trees/1427-all-elements-in-two-binary-search-trees.java
@@ -15,10 +15,30 @@
  */
 class Solution {
     public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
+        List<Integer> arr1=new ArrayList<>();
+        List<Integer> arr2=new ArrayList<>();
         List<Integer> res=new ArrayList<>();
-        inorder(root1,res);
-        inorder(root2,res);
-        Collections.sort(res);
+        inorder(root1,arr1);
+        inorder(root2,arr2);
+        int i=0,j=0;
+        while(i<arr1.size() && j<arr2.size()){
+            if(arr1.get(i)<arr2.get(j)){
+                res.add(arr1.get(i));
+                i++;
+            }
+            else{
+                res.add(arr2.get(j));
+                j++;
+            }
+        }
+        while(i<arr1.size()){
+            res.add(arr1.get(i));
+            i++;
+        }
+        while(j<arr2.size()){
+            res.add(arr2.get(j));
+            j++;
+        }
         return res;
     }
     public void inorder(TreeNode root,List<Integer> arr){
